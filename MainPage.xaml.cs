c039998a133d5e8420f98d008b30d3e5a6fb23d3
Bastien <chevalierbastien770@gmail.com>
2025-03-17 09:45:42 +0100

    Ajoutez des fichiers projet.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TrajetCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
