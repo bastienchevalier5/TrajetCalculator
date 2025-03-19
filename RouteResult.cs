1f2f710b178906d4b6db6002c3bfa89c57644673
AryaMerlet <151627770+AryaMerlet@users.noreply.github.com>
2025-03-19 10:14:11 +0100

    new algo retourn distance + objet

---
+++ RouteResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrajetCalculator
+{
+    public class RouteResult
+    {
+        public double TotalDistance { get; set; }
+        public Dictionary<string, Dictionary<string, double>> DistanceMatrix { get; set; }
+        public List<Commune> Route { get; set; }
+
+
+        public RouteResult()
+        {
+            TotalDistance = 0;
+            DistanceMatrix = new Dictionary<string, Dictionary<string, double>>();
+            Route = new List<Commune>();
+        }
+    }
+}
